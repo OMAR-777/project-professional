c2cc35b2b84e4449490d151a45fd26bbe6a85079
OMAR-777 <63660382+OMAR-777@users.noreply.github.com>
2021-04-12 07:37:34 +0300

    fixing titles

--- resReport.php
+++ resReport.php
@@ -110,7 +110,7 @@ function printResReport()
     <script src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.10.24/js/dataTables.bootstrap4.min.js"></script>
 
-    <title>Tasks Resources | Project Professional</title>
+    <title>Resources Report | Project Professional</title>
 </head>
 
 <body>
--- tasksReport.php
+++ tasksReport.php
@@ -122,7 +122,7 @@ function printTasksReport()
     <script src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.10.24/js/dataTables.bootstrap4.min.js"></script>
 
-    <title>Tasks Resources | Project Professional</title>
+    <title>Tasks Report | Project Professional</title>
 </head>
 
 <body>
