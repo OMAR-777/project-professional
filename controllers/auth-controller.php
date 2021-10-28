4442077591c40392df6cec37ad9653763f564847
OMAR-777 <63660382+OMAR-777@users.noreply.github.com>
2021-10-28 06:50:08 +0300

    changing directories

---
+++ controllers/auth-controller.php
@@ -0,0 +1,183 @@
+<?php
+session_start();
+
+if($_SERVER["DOCUMENT_ROOT"]=='C:/xampp/htdocs'){//if local host
+    define('ROOT', dirname(__DIR__));
+}else{
+    define('ROOT', $_SERVER["DOCUMENT_ROOT"]);
+}
+
+require ROOT.'/config/db.php';
+
+
+
+
+$errors = array(); //will be available on signup
+$username = "";
+$email = "";
+
+//if user click on the sign up button
+if (isset($_POST['signup-btn'])) {
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $passwordConf = $_POST['passwordConf'];
+
+    //validation
+    if (empty($username)) {
+        $errors['username'] = "Username required";
+    }
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) { //IF TRUE INVALID
+        $errors['email'] = "Email address is invalid";
+    }
+    if (empty($email)) {
+        $errors['email'] = "Email required";
+    }
+    if (empty($password)) {
+        $errors['password'] = "Password required";
+    }
+    if ($password !== $passwordConf) {
+        $errors['password'] = "The two passwords do not match";
+    }
+
+    $emailQuery = "SELECT * FROM users WHERE email=? LIMIT 1"; // (?) using prepared statements, LIMIT1 if u see one record then stop searching
+    $stmt = $conn->prepare($emailQuery);
+    $stmt->bind_param('s', $email); //s- string , add email instead of ?
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $userCount = $result->num_rows;
+    $stmt->close();
+
+    if ($userCount > 0) {
+        $errors['email'] = "Email already exists";
+    }
+
+    $userQuery = "SELECT * FROM users WHERE username=? LIMIT 1"; // (?) using prepared statements, LIMIT1 if u see one record then stop searching
+    $stmt = $conn->prepare($userQuery);
+    $stmt->bind_param('s', $username); //s- string , add email instead of ?
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $userCount = $result->num_rows;
+    $stmt->close();
+
+    if ($userCount > 0) {
+        $errors['username'] = "Username already exists";
+    }
+
+    if (count($errors) === 0) {
+        $password = password_hash($password, PASSWORD_DEFAULT); //ENCRYPT
+        $token = bin2hex(random_bytes(50)); // unique random string of length 100
+        $verified = false;
+
+        $sql = "INSERT INTO users (username, email, verified, token, password) VALUES (?, ?, ?, ?, ?)";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param('ssbss', $username, $email, $verified, $token, $password); //string string boolean.... 
+        if ($stmt->execute()) {
+            //login user
+            $user_id = $conn->insert_id; // get the last inserted id from conn object
+            $_SESSION['id'] = $user_id;
+            $_SESSION['username'] = $username;
+            $_SESSION['email'] = $email;
+            $_SESSION['verified'] = $verified;
+            $_SESSION['avatar'] = 'default.png';
+
+
+
+            //flash message
+            $_SESSION['message'] = "You are now logged in";
+            $_SESSION['alert-class'] = "alert-success";
+            header('location: index.php');
+            exit(); // not execute any other thing from here
+
+        } else {
+            $errors['db_error'] = "Database error: failed to register";
+        }
+    }
+}
+
+//if user clicks on the login button
+if (isset($_POST['login-btn'])) {
+    $username = $_POST['username']; //either email or username
+    $password = $_POST['password'];
+
+    //validation
+    if (empty($username)) {
+        $errors['username'] = "Username required";
+    }
+    if (empty($password)) {
+        $errors['password'] = "Password required";
+    }
+    if (count($errors) === 0) {
+        $sql = "SELECT * FROM users WHERE (email=? OR username=?) AND EXTERNAL_TYPE IS NULL LIMIT 1";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param('ss', $username, $username); //user might enter either email or username
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $user = $result->fetch_assoc(); //return a user full ROW with * coloumns as an associative array
+
+        //check if the user has the same password as registered
+
+        if ($user != null && password_verify($password, $user['password'])) { //check if the given pwd from user match the encrypted pwd stored in DB
+            //login success
+            //login user
+            settingUserSession($user);
+            //flash message
+            $_SESSION['message'] = "You are now logged in";
+            $_SESSION['alert-class'] = "alert-success";
+            header('location: index.php');
+            exit(); // dont execute any other thing from here
+
+        } else {
+            $errors['login_fail'] = "Wrong username or password!";
+        }
+    }
+}
+
+//logout user
+if (isset($_GET['logout'])) {
+    session_destroy();
+    unset($_SESSION['id']);
+    unset($_SESSION['username']);
+    unset($_SESSION['email']);
+    unset($_SESSION['verified']);
+    unset($_SESSION['avatar']);
+
+    header('location: login.php?loggedOut=We hope to see you again ❤');
+    exit();
+}
+function settingUserSession($user)
+{
+    $_SESSION['id'] = $user['id'];
+    $_SESSION['username'] = $user['username'];
+    $_SESSION['email'] = $user['email'];
+    $_SESSION['verified'] = $user['verified'];
+    $_SESSION['avatar'] = $user['avatar'];
+}
+
+//verify user by token
+function verifyUser($token)
+{ // each user has a unique token
+    global $conn;
+    $sql = "SELECT * FROM users WHERE token='$token' LIMIT 1";
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result) > 0) {
+        $user = mysqli_fetch_assoc($result); // get row as assoc array
+        $update_query = "UPDATE users SET verified=1 WHERE token='$token'";
+
+        if (mysqli_query($conn, $update_query)) { //if executed proberly user is updated/activated
+            //log user in
+            settingUserSession($user);
+            $_SESSION['verified']=1;
+            //flash message
+            $_SESSION['message'] = "Your email address is successfully verified!";
+            $_SESSION['alert-class'] = "alert-success";
+            header('location: index.php');
+            exit(); // not execute any other thing from here
+
+
+        }
+    } else { // if token not found from any user
+        echo 'User not found';
+    }
+}
--- resources.php
+++ resources.php
@@ -188,7 +188,7 @@ function printRes()
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="css/style.css">
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 
